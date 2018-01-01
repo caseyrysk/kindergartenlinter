a925790a2c2cb1947d66139eda79572f01d21c1c
Ryan Casey <ryan.casey@skapatech.com>
2018-01-01 15:49:53 -0500

    fixed unit tests; removed comments

--- src/utils/stringCleaning.js
+++ src/utils/stringCleaning.js
@@ -20,29 +20,16 @@ export async function replaceBadWords(inputText, badTokens, replacementTokens) {
     const getAntonymPromises = inputText.split(' ').map(
         async (word, index) => {
             const fallbackWord = index < replaceMask.length ? replaceMask[index] : replaceMask[replaceMask.length - 1];
-                // console.log("getAntonymPromises::" + fallbackWord); // eslint-disable-line
-                return await getAntonym(word, fallbackWord).then(replacement => {
-                //     console.log("replaceBadWords::" + x); // eslint-disable-line
-                //     return x;
-                // });
-                // console.log("replaceBadWords::" + replacement); // eslint-disable-line
-
+            return await getAntonym(word, fallbackWord).then(replacement => {
                 return word.replace(regEx, replacement);
             });
         }
-    ); // .join(' ');
-    // Wait for all Promises to complete
+    );
 
-    // console.log("getAntonymPromises::" + getAntonymPromises); // eslint-disable-line
     await Promise.all(getAntonymPromises)
         .then(results => {
-            // console.log("wtf:results::" + results); // eslint-disable-line
             result = results.join(' ');
-        })
-        .catch(e => {
-            console.error(e); // eslint-disable-line
         });
-    // result = getAntonymPromises;
 
     return result;
 }
--- src/utils/stringCleaning.spec.js
+++ src/utils/stringCleaning.spec.js
@@ -7,33 +7,27 @@ describe('String Cleaning', () => {
     });
   });
 
-  // describe('simple replace', () => {
-  //   it('returns flower for fart', async() => {
-  //     expect(replaceBadWords('simple fart')).toEqual('simple flower');
-  //   });
-  // });
+  describe('simple replace', () => {
+    it('returns flower for fart', async() => {
+      return expect(replaceBadWords('simple fart', ['fart'], ['flower'])).resolves.toEqual('simple flower');
+    });
+  });
 
-  // describe('plural word replace', () => {
-  //   it('returns flower for farts', async () => {
-  //     expect(replaceBadWords('simple farts')).toEqual('simple flowers');
-  //   });
-  // });
+  describe('plural word replace', () => {
+    it('returns flower for farts', async () => {
+      return expect(replaceBadWords('simple farts', ['fart'], ['flower'])).resolves.toEqual('simple flowers');
+    });
+  });
 
-  // describe('plural word replace', () => {
-  //   it('returns flower for stupid', async() => {
-  //     expect(replaceBadWords('simple stupid', ['stupid', 'dumb'], ['flower', 'flower'])).toEqual('simple flower');
-  //   });
-  // });
+  describe('plural word replace', () => {
+    it('returns smart for stupid', async() => {
+      return expect(replaceBadWords('simple stupid', ['stupid', 'dumb'], ['flower', 'flower'])).resolves.toEqual('simple smart');
+    });
+  });
 
-  // describe('plural word replace', async() => {
-  //   it('returns flower for stupid', () => {
-  //     expect(replaceBadWords('simple stupid', ['stupid', 'dumb'], ['foobar'])).toEqual('simple foobar');
-  //   });
-  // });
-
-  // describe('unique replace', () => {
-  //   it('returns foo bar for dumb stupid', async() => {
-  //     expect(replaceBadWords('dumb stupid', ['stupid', 'dumb'], ['foo', 'bar'])).toEqual('foo bar');
-  //   });
-  // });
+  describe('unique replace', () => {
+    it('returns foo bar for dumb stupid', async() => {
+      return expect(replaceBadWords('dumb stupid', ['stupid', 'dumb'], ['foo', 'bar'])).resolves.toEqual('foo smart');
+    });
+  });
 });
